a94bff2ae9d8b1ac8f75f4ec08971cc3081df4a1
Eric Harris-Braun <eric@harris-braun.com>
2022-02-10 15:01:50 -0500

    update to hc 125

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
-  holonixRev = "1798d764873d53cfce576860b707af35ef4ba7cd";
+  holonixRev = "9ddfc68aaf665f13e674a5382946f2e262538abe";
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_124";
+    holochainVersionId = "v0_0_125";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
