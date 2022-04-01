b816406c919cef51635f32f100deec3a924c9732
Patrick Winfield <patrick.winfield@holo.host>
2022-04-01 16:13:01 +0000

    WIP text selection popup

--- ui/lib/src/elements/how-document-section.ts
+++ ui/lib/src/elements/how-document-section.ts
@@ -24,8 +24,25 @@ import {Marked} from "@ts-stack/markdown";
                 ${this.section?.name}
                 ${this.sectionTypeMarker()}
             </div>
-            <div>${sectionValue(this.section, this.index)}</div>
+            <div @pointerup=${this.onEndSelection}>${sectionValue(this.section, this.index)}</div>
+            <template id="template${this.index}"><span class="addCommentControl" id="addCommentControl"></span></template>
         </div>`
+    }
+
+    private onEndSelection() {
+        const template = document.querySelector('#template'+this.index)
+        const control = document.importNode((<any> template).content, true)?.childNodes[0];
+        const selection = document.getSelection()
+        const text = selection?.toString();
+        if (text) {
+            let rect = selection?.getRangeAt(0).getBoundingClientRect();
+            control.style.top = `calc(${rect?.top}px - 48px)`;
+            control.style.left = `calc(${rect?.left}px + calc(${rect?.width}px / 2) - 40px)`;
+            control['text']= text; 
+            document.body.appendChild(control);
+        }
+
+        console.log("onEndSelection: " + text);
     }
 
     private sectionTypeMarker() {
@@ -55,6 +72,25 @@ import {Marked} from "@ts-stack/markdown";
             font-weight: bold;
             margin-bottom: 2px;
         }
+        .addCommentControl {
+            background-image: url("data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width='40px' height='40px'><foreignObject width='40px' height='40px'><div xmlns='http://www.w3.org/1999/xhtml' style='width:40px;height:40px;line-height:40px;text-align:center;color:transparent;text-shadow: 0 0 yellow, 2px 4px black, -1px -1px black;font-size:35px;'>💬</div></foreignObject></svg>");
+            cursor: pointer;
+            position: absolute;
+            width: 40px;
+            height: 40px;
+        }
+        .addCommentControl::before{
+            background-color: black;
+            color: white;
+            content: " add comment ";
+            display: block;
+            font-weight: bold;
+            margin-left: 37px;
+            margin-top: 6px;
+            padding: 2px;
+            width: max-content;
+            height: 20px;
+        }        
     `    
 }
 
