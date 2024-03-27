25fa9d77c7f0dac1046323ff950e068d9d500483
Eric Harris-Braun <eric@harris-braun.com>
2024-03-26 20:22:43 -0400

    update dev apps

--- we_dev/config.ts
+++ we_dev/config.ts
@@ -38,18 +38,18 @@ export default defineConfig({
           registeringAgent: 1,
           joiningAgents: [2],
         },
-        // {
-        //   name: 'gamez',
-        //   instanceName: 'gamez',
-        //   registeringAgent: 1,
-        //   joiningAgents: [2],
-        // },
-        // {
-        //   name: 'notebooks',
-        //   instanceName: 'notebooks',
-        //   registeringAgent: 1,
-        //   joiningAgents: [2],
-        // },
+        {
+          name: 'gamez',
+          instanceName: 'gamez',
+          registeringAgent: 1,
+          joiningAgents: [2],
+        },
+        {
+          name: 'notebooks',
+          instanceName: 'notebooks',
+          registeringAgent: 1,
+          joiningAgents: [2],
+        },
       ],
     },
   ],
@@ -68,31 +68,31 @@ export default defineConfig({
         uiPort: 8888,
       },
     },
-    // {
-    //     name: 'gamez',
-    //     subtitle: 'play!',
-    //     description: 'Real-time games based on syn',
-    //     icon: {
-    //       type: "https",
-    //       url: "https://raw.githubusercontent.com/holochain-apps/gamez/main/we_dev/gamez_icon.svg"
-    //     },
-    //     source: {
-    //       type: "https",
-    //       url: "https://github.com/holochain-apps/gamez/releases/download/v0.3.100/gamez.webhapp"
-    //     },
-    //   },
-    //   {
-    //   name: 'notebooks',
-    //   subtitle: 'Collaborative note taking',
-    //   description: 'Real-time notetaking based on syn',
-    //   icon: {
-    //     type: 'https',
-    //     url: 'https://lightningrodlabs.org/projects/notebooks.png',
-    //   },
-    //   source: {
-    //     type: 'https',
-    //     url: 'https://github.com/lightningrodlabs/notebooks/releases/download/v0.2.0/notebooks.webhapp',
-    //   },
-    // },
+    {
+        name: 'gamez',
+        subtitle: 'play!',
+        description: 'Real-time games based on syn',
+        icon: {
+          type: "https",
+          url: "https://raw.githubusercontent.com/holochain-apps/gamez/main/we_dev/gamez_icon.svg"
+        },
+        source: {
+          type: "https",
+          url: "https://github.com/holochain-apps/gamez/releases/download/v0.4.1/gamez.webhapp"
+        },
+      },
+      {
+      name: 'notebooks',
+      subtitle: 'Collaborative note taking',
+      description: 'Real-time note-taking based on syn',
+      icon: {
+        type: 'https',
+        url: 'https://lightningrodlabs.org/projects/notebooks_logo.svg',
+      },
+      source: {
+        type: 'https',
+        url: 'https://github.com/lightningrodlabs/notebooks/releases/download/v0.2.4/notebooks.webhapp',
+      },
+    },
   ],
 });
